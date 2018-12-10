c9181340d1f431f4b88cfa4eedc60e67674e3128
irishhq <iris_hhqian@163.com>
2018-12-10 22:34:50 +0800

    入口文件

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'lib-flexible/flexible.js'
+
+Vue.config.productionTip = false
+
+/*使用axios来代替$http, axios支持session*/
+import axios from "axios"//引入axios库,vueRource不支持session
+axios.defaults.withCredentials = true;//记录session编号,ajax默认不保存session得值.
+Vue.prototype.axios = axios;//相当于注册
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
