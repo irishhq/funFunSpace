090ed0953b59275409f0101505a179d7a5275a8c
irishhq <iris_hhqian@163.com>
2018-12-10 22:37:15 +0800

    用户登录注册相关组件

---
+++ src/components/login/Login.vue
@@ -0,0 +1,126 @@
+<template>
+    <div class="app-login">
+        <div class="wrap-login">
+            <form action="/" >
+                <div class="block">
+                    <img src="../../assets/img/login_signin/uname.png" alt="用户名" v-model="uname">
+                    <input type="text" placeholder="请输入昵称"  v-model="uname">
+                </div>
+                <div class="block">
+                    <img src="../../assets/img/login_signin/upwd.png" alt="密码">
+                    <input type="password" placeholder="请输入密码"  v-model="upwd">
+                </div>
+            </form>
+            <div class="block">
+                <a href="#">忘记密码?</a>
+                <a href="#">用户注册</a>  
+            </div> 
+            <button class="block" @click="handleLogin">登录</button>
+        </div>
+    </div>
+</template>
+
+<script>
+// import { Toast } from "mint-ui";
+export default {
+  data() {
+    return {
+      uname: "",
+      upwd: ""
+    };
+  },
+  methods: {
+    handleLogin() {
+      //1. 获取参数
+      console.log(this.uname);
+      var u = this.uname;
+      var p = this.upwd;
+      //2. 正则表达式验证
+
+      //3. 发送请求
+      var params = {
+        uname: u,
+        upwd: p
+      };
+
+      var url = "http://127.0.0.1:3000/login";
+      this.axios.get(url, { params }).then(result => {
+        console.log(result);
+        var obj = result.data;
+        if (obj.code == -1) {
+          // Toast(obj.msg);
+          console.log(obj.msg);
+        } else if (obj.code == 1) {
+          console.log(obj.msg);
+          this.$router.push("/home");
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "../../assets/stylesheet/common.scss";
+// position & size
+$border-raduis: $block-height / 2;
+$margin-top-large: 70px;
+
+.app-login {
+  font-size: $font-lg;
+  width: 100%;
+  height: 100%;
+  //嵌套属性语法
+  background: #9dc3da url("../../assets/img/login_signin/bg_login.jpg")
+    no-repeat {
+    size: cover;
+  }
+}
+.app-login .wrap-login {
+  height: 100%;
+  @include displayFlex(center, center, column);
+  & {
+    // wrap-login下的公共样式
+    .block {
+      @extend .block-size;
+      border-radius: $border-raduis;
+      margin-top: $margin-top;
+    }
+    .block:first-child {
+      margin-top: 0;
+    }
+    // 用户输入表单
+    form > div {
+      border: 2px solid $white;
+      input {
+        background: transparent;
+        margin-left: $margin-sm;
+        font-size: $font-md;
+      }
+      img {
+        @extend .icon-size;
+        margin-left: $margin-md;
+        margin-bottom: -5px;
+      }
+    }
+    // 忘记密码&用户注册
+    > div[class="block"] {
+      @include clear-mp;
+      @include displayFlex(space-between, center, row);
+      margin-top: $margin-top;
+      font-size: $font-sm;
+      height: $font-sm;
+      line-height: $font-sm;
+      & > a {
+        margin: 0 $margin-md;
+      }
+    }
+    // 登录按钮
+    > button[class="block"] {
+      margin-top: $margin-top-large;
+      background: $blue;
+      font-size: $font-lg;
+    }
+  }
+}
+</style>
---
+++ src/components/login/ResetPwd.vue
@@ -0,0 +1,142 @@
+<template>
+    <div class="app-reset-pwd">
+        <!-- 导航栏 -->
+        <div class="nav-wrap">
+            <div class="nav-bar">
+                <span><a href="">&lt;</a></span>
+                <h3>忘记密码</h3>
+                <span><a href=""></a></span>
+            </div>
+        </div>
+        
+        <!-- 忘记密码表单 -->
+        <form action="/" >
+            <div class="block">
+                <img src="../../assets/img/login_signin/phone.png">
+                <input type="text" placeholder="请输入手机号码">
+            </div>
+            <div class="block">
+                <img src="../../assets/img/login_signin/nick_name.png">
+                <input type="password" placeholder="请输入昵称">
+            </div>
+            <div class="block">
+                <div class="idcode">
+                    <img src="../../assets/img/login_signin/idcode.png">
+                    <input class="" type="password" placeholder="请输入验证码">
+                </div>
+                <button class="get-idcode">获取验证码</button>
+            </div>
+        </form>
+        <!-- 确定,提交表单按钮 -->
+        <div>
+
+        </div>
+        <button type="submit" class="block">
+            确定
+        </button>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "../../assets/stylesheet/common.scss";
+input::placeholder {
+  color: $dark-gray;
+}
+.app-reset-pwd {
+  width: 100%;
+  height: 100%;
+  background: $light-gray;
+}
+// 导航栏
+.app-reset-pwd .nav-wrap {
+  height: $nav-height;
+  background: {
+    color: #9dc3da;
+    image: url("../../assets/img/login_signin/status_bar.png");
+    size: cover;
+  }
+  .nav-bar {
+    font-size: $font-lg;
+    padding-top: 30px;
+    @include displayFlex(space-between, center, row);
+    h3 {
+      font: {
+        size: $font-lg;
+        weight: lighter;
+      }
+    }
+    span {
+      width: 27px;
+      height: 45px;
+      margin-left: 42px;
+    }
+  }
+}
+// 表单
+.app-reset-pwd > form {
+  margin-top: 18px;
+  background: $white;
+  padding: 30px 0;
+  .block {
+    @extend .block-size;
+    margin-top: $margin-top;
+    margin: 0 auto;
+  }
+  // 用户输入表单
+  & > div {
+    border-bottom: 2px solid $light-gray;
+    position: relative;
+    input {
+      background: transparent;
+      margin-left: $margin-sm;
+      font-size: $font-md;
+      color: $dark-gray;
+    }
+    img {
+      @extend .icon-size;
+      margin: {
+        left: $margin-sm;
+        bottom: -5px;
+      }
+    }
+
+    &:last-child {
+      border: none;
+    }
+    //获取验证码
+    .get-idcode {
+      width: $block-width/2 - 30px;
+      float: right;
+      background-color: $dark-gray;
+      border-radius: 8px;
+      height: 60px;
+      position: relative;
+      margin-top: -62px;
+      font: {
+        size: $font-sm;
+        weight: lighter;
+      }
+    }
+  }
+  // 验证码边框重置;
+  .idcode {
+    @include displayFlex(flex-start, center, row);
+    height: $block-height;
+    width: $block-width/2;
+    margin-left: 0;
+    border-bottom: 2px solid $light-gray;
+  }
+}
+// 登录按钮
+.app-reset-pwd button[class="block"] {
+  @extend .block-size;
+  margin: $margin-md $margin-lg;
+  background: $blue;
+  font-size: $font-lg;
+  border-radius: 12px;
+}
+</style>
