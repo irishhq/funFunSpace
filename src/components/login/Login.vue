b277cc7c66ef77bf04a496e1598b9fa9c4ffa261
irishhq <iris_hhqian@163.com>
2018-12-11 22:26:28 +0800

    login component, update styles and add funcs

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,23 +1,25 @@
 <template>
-    <div class="app-login">
-        <div class="wrap-login">
-            <form action="/" >
-                <div class="block">
-                    <img src="../../assets/img/login_signin/uname.png" alt="用户名" v-model="uname">
-                    <input type="text" placeholder="请输入昵称"  v-model="uname">
-                </div>
-                <div class="block">
-                    <img src="../../assets/img/login_signin/upwd.png" alt="密码">
-                    <input type="password" placeholder="请输入密码"  v-model="upwd">
-                </div>
-            </form>
-            <div class="block">
-                <a href="#">忘记密码?</a>
-                <a href="#">用户注册</a>  
-            </div> 
-            <button class="block" @click="handleLogin">登录</button>
-        </div>
+  <div class="app-login">
+    <div class="bg">
     </div>
+    <div class="wrap-login">
+      <form action="/">
+        <div class="block">
+          <img src="../../assets/img/login_signin/uname.png" alt="用户名" v-model="uname">
+          <input type="text" placeholder="请输入昵称" v-model="uname">
+        </div>
+        <div class="block">
+          <img src="../../assets/img/login_signin/upwd.png" alt="密码">
+          <input type="password" placeholder="请输入密码" v-model="upwd">
+        </div>
+      </form>
+      <div class="block">
+        <a href="#">忘记密码?</a>
+        <a href="#">用户注册</a>
+      </div>
+      <button class="block" @click="handleLogin">登录</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -29,6 +31,7 @@ export default {
       upwd: ""
     };
   },
+
   methods: {
     handleLogin() {
       //1. 获取参数
@@ -44,17 +47,21 @@ export default {
       };
 
       var url = "http://127.0.0.1:3000/login";
-      this.axios.get(url, { params }).then(result => {
-        console.log(result);
-        var obj = result.data;
-        if (obj.code == -1) {
-          // Toast(obj.msg);
-          console.log(obj.msg);
-        } else if (obj.code == 1) {
-          console.log(obj.msg);
-          this.$router.push("/home");
-        }
-      });
+      this.axios
+        .get(url, {
+          params
+        })
+        .then(result => {
+          console.log(result);
+          var obj = result.data;
+          if (obj.code == -1) {
+            // Toast(obj.msg);
+            console.log(obj.msg);
+          } else if (obj.code == 1) {
+            console.log(obj.msg);
+            this.$router.push("/home");
+          }
+        });
     }
   }
 };
@@ -67,18 +74,33 @@ $border-raduis: $block-height / 2;
 $margin-top-large: 70px;
 
 .app-login {
+  z-index: 999;
   font-size: $font-lg;
   width: 100%;
   height: 100%;
-  //嵌套属性语法
-  background: #9dc3da url("../../assets/img/login_signin/bg_login.jpg")
-    no-repeat {
-    size: cover;
+  input,
+  button,
+  a,
+  input::placeholder {
+    color: $white;
+  }
+  .bg {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: -99;
+    filter: blur(5px);
+    background: #9dc3da url("../../assets/img/login_signin/bg_login.jpg")
+      no-repeat {
+      size: cover;
+    }
   }
 }
+
 .app-login .wrap-login {
   height: 100%;
   @include displayFlex(center, center, column);
+
   & {
     // wrap-login下的公共样式
     .block {
@@ -86,23 +108,28 @@ $margin-top-large: 70px;
       border-radius: $border-raduis;
       margin-top: $margin-top;
     }
+
     .block:first-child {
       margin-top: 0;
     }
+
     // 用户输入表单
     form > div {
       border: 2px solid $white;
+
       input {
         background: transparent;
         margin-left: $margin-sm;
         font-size: $font-md;
       }
+
       img {
         @extend .icon-size;
         margin-left: $margin-md;
         margin-bottom: -5px;
       }
     }
+
     // 忘记密码&用户注册
     > div[class="block"] {
       @include clear-mp;
@@ -111,10 +138,12 @@ $margin-top-large: 70px;
       font-size: $font-sm;
       height: $font-sm;
       line-height: $font-sm;
+
       & > a {
         margin: 0 $margin-md;
       }
     }
+
     // 登录按钮
     > button[class="block"] {
       margin-top: $margin-top-large;
