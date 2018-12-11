41faa03a75fc48e39593e8ab10bd872a850e5c11
irishhq <iris_hhqian@163.com>
2018-12-11 22:24:05 +0800

    add imgs route

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Login from '@/components/login/Login'
-import ResetPwd from '@/components/login/ResetPwd'
-import Register from '@/components/login/Register'
+import login from '@/components/login/login'
+import resetPwd from '@/components/login/resetPwd'
+import register from '@/components/login/register'
+import home from '@/components/tabbar/home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    { path: '/login', component: Login },
-    { path: '/resetPwd', component: ResetPwd },
-    { path: '/register', component: Register }
+    { path: '/', redirect: '/home' },//重定向,指向home组件
+    { path: '/home', component: home },
+    { path: '/login', component: login },
+    { path: '/resetPwd', component: resetPwd },
+    { path: '/register', component: register }
   ]
 })
