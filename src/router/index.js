8fa71f980806ef8b6ac5390ac0929c5b76a716d0
irishhq <iris_hhqian@163.com>
2018-12-13 22:49:39 +0800

    update routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,38 @@ import Router from 'vue-router'
 import login from '@/components/login/login'
 import resetPwd from '@/components/login/resetPwd'
 import register from '@/components/login/register'
-import home from '@/components/tabbar/home'
+import Tabbar from '@/components/Tabbar'
+import Home from '@/components/tabbar/Home'
+import Purchase from '@/components/tabbar/Purchase'
+import Mime from '@/components/tabbar/Mime'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    { path: '/', redirect: '/home' },//重定向,指向home组件
-    { path: '/home', component: home },
+    { path: '/', redirect: '/tabbar/home' },//重定向,指向home组件
+    {
+      path: '/tabbar',
+      name: 'Tabbar',
+      component: Tabbar,
+      children: [
+        {
+          path: 'home',
+          name: 'Home',
+          component: Home
+        },
+        {
+          path: 'purchase',
+          name: 'Purchase',
+          component: Purchase
+        },
+        {
+          path: 'mime',
+          name: 'Mime',
+          component: Mime
+        }
+      ]
+    },
     { path: '/login', component: login },
     { path: '/resetPwd', component: resetPwd },
     { path: '/register', component: register }
